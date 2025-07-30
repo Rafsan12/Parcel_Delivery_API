faf743c46dc52e69108cc38f40d84bb6fddef241
Rafsan12 <rafsan.web.2022@gmail.com>
2025-07-30 19:29:49 +0600

    user interface and model complete

---
+++ src/app/Modules/User/user.interface.ts
@@ -0,0 +1,29 @@
+import { Types } from "mongoose";
+
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+
+export interface IAuthProvider {
+  provider: "google" | "credentials";
+  providerId: string;
+}
+
+export enum Role {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  SENDER = "SENDER",
+  RECEIVER = "RECEIVER",
+}
+
+export interface IUser {
+  _id?: Types.ObjectId;
+  name: string;
+  email: string;
+  password?: string;
+  role: Role;
+  auth: IAuthProvider[];
+  isActive: IsActive;
+}
