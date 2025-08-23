934d6332b5e38b0f637a1a6dc2f1e7be09796c92
Rafsan12 <rafsan.web.2022@gmail.com>
2025-08-23 22:47:56 +0600

    payment interface,models and  Transaction and Rollback part complete

--- src/app/Modules/parcel/parcel.service.ts
+++ src/app/Modules/parcel/parcel.service.ts
@@ -1,33 +1,94 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import httpStatus from "http-status-codes";
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import AppError from "../../errorHelpers/AppError";
 import { calculatePrice } from "../../utils/calculatePrice";
+import { PAYMENT_STATUS } from "../payment/payment.interface";
+import { Payment } from "../payment/payment.models";
 import { Role } from "../User/user.interface";
 import { User } from "../User/user.model";
 import { IParcel, ParcelStatus } from "./parcel.interface";
 import { Parcel } from "./parcel.model";
 
-const createParcel = async (payload: Partial<IParcel>) => {
-  const senderUser = await User.findById(payload.sender);
-  if (!senderUser) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Sender user not found");
-  }
+const getTransactionId = () => {
+  return `tran_${Date.now()}_${Math.floor(Math.random() * 1000)}`;
+};
 
-  if (senderUser.role === Role.RECEIVER) {
-    throw new AppError(
-      httpStatus.BAD_REQUEST,
-      "Receivers cannot create parcels"
+const createParcel = async (payload: Partial<IParcel>, userId: string) => {
+  const transactionId = getTransactionId();
+
+  const session = await Parcel.startSession();
+  session.startTransaction();
+
+  try {
+    const user = await User.findById(userId);
+    const senderUser = await User.findById(payload.sender);
+
+    if (!senderUser) {
+      throw new AppError(httpStatus.BAD_REQUEST, "Sender user not found");
+    }
+
+    if (senderUser.role === Role.RECEIVER) {
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        "Receivers cannot create parcels"
+      );
+    }
+
+    // 🔑 Receiver email mandatory check
+    if (!payload.customerEmail) {
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        "Receiver email (customerEmail) is required"
+      );
+    }
+
+    const totalPrice = (payload.price = calculatePrice(
+      payload.weight!,
+      payload.deliveryArea!
+    ));
+
+    // payload.status = ParcelStatus.PENDING;
+
+    const parcel = await Parcel.create(
+      [
+        {
+          user: userId,
+          status: ParcelStatus.PENDING,
+          ...payload,
+        },
+      ],
+      { session }
     );
+
+    const payment = await Payment.create(
+      [
+        {
+          parcel: parcel[0]._id,
+          status: PAYMENT_STATUS.UNPAID,
+          transactionId: transactionId,
+          amount: totalPrice,
+        },
+      ],
+      { session }
+    );
+
+    const updateParcel = await Parcel.findByIdAndUpdate(
+      parcel[0]._id,
+      {
+        payment: payment[0]._id,
+      },
+      { new: true, runValidators: true, session }
+    );
+
+    await session.commitTransaction();
+    session.endSession();
+    return updateParcel;
+  } catch (error: any) {
+    await session.abortTransaction();
+    session.endSession();
+    throw error;
   }
-
-  if (!payload.price) {
-    payload.price = calculatePrice(payload.weight!, payload.deliveryArea!);
-  }
-  payload.status = ParcelStatus.PENDING;
-
-  const parcel = await Parcel.create(payload);
-
-  return parcel;
 };
 
 const updateParcelStatus = async (
---
+++ src/app/Modules/payment/payment.models.ts
@@ -0,0 +1,26 @@
+import { model, Schema } from "mongoose";
+import { IPayment, PAYMENT_STATUS } from "./payment.interface";
+
+const paymentSchema = new Schema<IPayment>(
+  {
+    parcel: {
+      type: Schema.Types.ObjectId,
+      ref: "Parcel",
+      required: true,
+      unique: true,
+    },
+    transactionId: { type: String, required: true, unique: true },
+    amount: { type: Number, required: true },
+    paymentGateway: Schema.Types.Mixed,
+    status: {
+      type: String,
+      enum: Object.values(PAYMENT_STATUS),
+      default: PAYMENT_STATUS.UNPAID,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Payment = model<IPayment>("Payment", paymentSchema);
