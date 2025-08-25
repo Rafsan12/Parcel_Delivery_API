e0122489dffcd32fcb034ce4ae03ac2f550fab58
Rafsan12 <rafsan.web.2022@gmail.com>
2025-08-25 19:06:08 +0600

    changePassword and reset-password api complete

--- src/app/Modules/auths/auth.route.ts
+++ src/app/Modules/auths/auth.route.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../User/user.interface";
 
 const router = Router();
 
 router.post("/login", AuthController.credentialsLogin);
 router.post("/refreshToken", AuthController.getNewAccessToken);
 router.post("/log-out", AuthController.logout);
+router.post("/reset-password", checkAuth(...Object.values(Role)), AuthController.resetPassword);
+router.post("/change-password", checkAuth(...Object.values(Role)), AuthController.changePassword);
 
 export const AuthRouter = router;
